2a5b3dd1d46ff597f5face86ada74dded6c51d65
santiago <bassilsantiago@gmail.com>
2024-07-15 13:17:50 -0500

    Add get image function to get weather related background images

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ const feelsLike = document.querySelector(".feels-like");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 
+const bodyBackground = document.getElementsByTagName("body")
+
 async function getWeather(city){
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`,
     {mode: "cors"});
@@ -19,6 +21,20 @@ async function getWeather(city){
 
     return weather
 }
+
+async function GetBgImage(searchTerm){
+    const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&per_page=1&page=1&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k`
+
+    const response = await fetch(url, {mode: "cors"});
+    const imageResponse = await response.json();
+
+    console.log(imageResponse)
+    return imageResponse;
+}
+
+GetBgImage("ballons").then(result => {
+    console.log(result.results[0].urls.raw)
+})
 
 window.onload = getWeather("london").then(result => {
     condition.textContent = result.current.condition.text;
